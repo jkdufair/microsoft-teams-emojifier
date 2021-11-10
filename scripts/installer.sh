678c577c44750e4fc4743f50a3db074d1bb1e580
Jason Dufair <jase@dufair.org>
2021-11-10 10:33:32 -0500

    Electron install WIP

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+UNPACKED=./app.unpacked
+JS=$UNPACKED/main.bundle.js
+ASAR="/Applications/Microsoft Teams.app/Contents/Resources/app.asar"
+PAYLOAD=dist/emojifier.js
+
+rm -rf $UNPACKED
+
 #unpack the electron bootstrapper into the local directory
-asar extract ~/AppData/Local/Microsoft/Teams/current/resources/electron.asar ./electron.unpacked
+npx asar extract "$ASAR" $UNPACKED
 
 #If we have already been injected, remove the existing code before adding new stuff
-sed -i '/\/\*\{8\}BEGIN EMOJIINJECT\*\{8\}\//,/\/\*\{8\}END EMOJIINJECT\*\{8\}\//d' ./electron.unpacked/browser/chrome-extension.js
+#gsed -i '/\/\*\{8\}BEGIN EMOJIINJECT\*\{8\}\//,/\/\*\{8\}END EMOJIINJECT\*\{8\}\//d' $JS
 
 #Inject the emoji payload
-echo "/********BEGIN EMOJIINJECT********/" >> ./electron.unpacked/browser/chrome-extension.js;
-echo "app.on('browser-window-created', function (event, bWindow) {bWindow.webContents.executeJavaScript(\`" >> ./electron.unpacked/browser/chrome-extension.js;
+#echo "/********BEGIN EMOJIINJECT********/" >> $JS
+#echo "app.on('browser-window-created', function (event, bWindow) {bWindow.webContents.executeJavaScript(\`" >> $JS
 # this line used to force debgging inside electron
 # echo "app.on('browser-window-focus', function (event, bWindow) {bWindow.webContents.openDevTools();debugger;bWindow.webContents.executeJavaScript(\`" >> ./electron.unpacked/browser/chrome-extension.js;
 
 
 #Inject the source emoji server from an environment variable
-echo "window.EMOJI_API = '"$EMOJI_URL"';" >> ./electron.unpacked/browser/chrome-extension.js;
+#echo "window.EMOJI_API = '"$EMOJI_URL"';" >> $JS
 #sed is used here to escape any existing backtick quotes or escape characters. using those style of quotes for now because the payload comes in as multiline
 #in the future would be nice to have a minified single-line payload to inject
-sed 's/\\/\\\\/g' payload.js | sed 's/`/\\`/g' >> ./electron.unpacked/browser/chrome-extension.js
-echo "\`)})" >> ./electron.unpacked/browser/chrome-extension.js
-echo "/********END EMOJIINJECT********/" >> ./electron.unpacked/browser/chrome-extension.js;
+#gsed 's/\\/\\\\/g' $PAYLOAD | gsed 's/`/\\`/g' >> $JS
+#echo "\`)})" >> $JS
+#echo "/********END EMOJIINJECT********/" >> $JS
 
-#re-pack the whole app back into the install locations
-asar pack ./electron.unpacked ~/AppData/Local/Microsoft/Teams/current/resources/electron.asar
+#re-pack the whole app back into the install location
+sudo npx asar pack $UNPACKED "$ASAR"
